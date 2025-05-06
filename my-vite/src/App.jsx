335845fda75646805e3a4b68afbc642d4bbcc085
deepucutie <deepamshusoni238@gmail.com>
2025-05-06 15:55:26 +0530

    react

--- my-vite/src/App.jsx
+++ my-vite/src/App.jsx
@@ -4,11 +4,13 @@
 import Chai from "./chai"
 
 function App() {
+  const username="chai aur code"
   
 
   return (
     <div>
       <Chai/>
+      <h1>chai aur react {username}</h1>
       <h3>Test para</h3>
     </div>
    
--- my-vite/src/main.jsx
+++ my-vite/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp()
+{
+  return (
+    <div>
+
+      <h1>Custom app  !</h1>
+    </div>
+  )
+
+}
+/*const reactElement={
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  childern: 'Click me to vist google'
+}*/
+
+const anotherElement=
+(
+  <a href="http://google.com" target='_blank'>Visit google</a>
 )
+
+const anotherUser="Chai aur react"
+
+const reactElement = React.createElement(
+'a',
+{
+ href: 'https://google.com',target:'_blank'},
+ 'click me to visit google',
+  anotherUser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+reactElement 
+)
+
